19ff1bd199463708287baaf2647b4542b3e0b31d
Mike Tubinis <michaeltubinis@gmail.com>
2014-10-04 12:52:51 -0400

    Now i'm getting somewhere with this. Networking is becoming much easier now I have figured out what threads actually do.

--- Tic-Tac-Pi/client.py
+++ Tic-Tac-Pi/client.py
@@ -1,17 +1,58 @@
-import sys, socket,thread,select,pygame
+import sys, socket,thread,select,pygame,json,Queue
+from urllib2 import urlopen
 from time import sleep
+from threading import Thread
 
 def GetIp():
-    import json
-    from urllib2 import urlopen
-    ip = json.load(urlopen('http://httpbin.org/ip'))['origin']
-    return ip
+    return json.load(urlopen('http://httpbin.org/ip'))['origin']
 
+class ClientThreadSend(Thread):
+    def __init__(self,socket):
+        super(ClientThreadSend, self).__init__()
+        self.running = True
+        self.conn = socket
+        self.q = Queue.Queue()
+    def add(self,data):
+        self.q.put(data)
+    def stop(self):
+        self.conn.shutdown(socket.SHUT_RDWR)
+        self.conn.close()
+        self.running = False 
+    def run(self):
+        while self.running:
+            try:
+                if not self.q.empty():
+                    message = self.q.get(block=True, timeout=1)
+                    self.conn.send(message)
+            except socket.error, msg:
+                print("Socket error! %s" % msg)
+                pass
+        self.conn.close()
+class ClientThreadRecieve(Thread):
+    def __init__(self,socket):
+        super(ClientThreadRecieve, self).__init__()
+        self.running = True
+        self.conn = socket
+
+    def stop(self):
+        self.conn.shutdown(socket.SHUT_RDWR)
+        self.conn.close()
+        self.running = False 
+    def run(self):
+        while self.running:
+            try:
+                data = self.conn.recv(1024)
+                print(data)
+            except socket.error, msg:
+                print("Socket error! %s" % msg)
+                pass
+        self.conn.close()
+        
 class Client(object):
     def __init__(self):
         self.ip = GetIp()
-        self.timer = 0
         self.client = None
+        self.client1 = None
         self.connected = False
         self.connection_destination = ""
         self.username = ""
@@ -26,11 +67,18 @@ class Client(object):
             return
         try:
             if self.connected == False:
-                self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.connection_destination = server
                 self.username = username
 
-                self.client.connect((self.connection_destination, 6119))
+                client_socket.connect((self.connection_destination, 6119))
+                
+                self.client = ClientThreadSend(client_socket)
+                self.client.start()
+
+                self.client1 = ClientThreadRecieve(client_socket)
+                self.client1.start()
+                
                 self.send_message("_CONNECT_" + self.username,errorButton)
                 
                 self.connected = True
@@ -42,30 +90,16 @@ class Client(object):
     def disconnect_from_server(self,errorButton,error=False):
         if error == False:
             self.send_message("_DISCONNECT_" + self.username,errorButton)
-        self.client.shutdown(socket.SHUT_RDWR)
-        self.client.close()
+            
+        self.client.stop()
+        self.client1.stop()
         self.connected = False
 
     def send_message(self,message,errorButton):
         if self.client != None:       
-            self.client.sendall(message)
-            self.print_response(errorButton)
-        
-    def print_response(self,errorButton):
-        try:
-            data = self.client.recv(1024)
-            print(data)
-        except:
-            error = "Server was shut down"
-            errorButton.update_message(error)
-            self.disconnect_from_server(True)
-            pass
-        
+            self.client.add(message)
+      
     def update(self,errorButton,events):
-        self.timer += 1
-        if self.timer > 50:
-            self.send_message(".",errorButton)
-            self.timer = 0
         for event in events:
             if event.type == pygame.KEYDOWN:
                 try:
--- Tic-Tac-Pi/server.py
+++ Tic-Tac-Pi/server.py
@@ -1,85 +1,104 @@
-import socket,sys,select,thread
+import socket,select,Queue,json
+from threading import Thread
 from time import sleep
+from urllib2 import urlopen
+import sys
 
-def removekey(d, key):
-    r = dict(d)
-    del r[key]
-    return r
+clients = []
+
+def broadcast(source_socket,message,tosender=False):
+    if source_socket != None:
+        if tosender == True:
+            source_socket.send(message)
+        for i in clients:
+            if i.conn is not source_socket:
+                i.conn.send(message)
 
 def GetIp():
-    import json
-    from urllib2 import urlopen
-    ip = json.load(urlopen('http://httpbin.org/ip'))['origin']
-    return ip
+    return json.load(urlopen('http://httpbin.org/ip'))['origin']
 
-class WaitingClient():
-    def __init__(self,conn,addr):
-        self.conn = conn
-        self.addr = addr
-        
-class Server(object):
+def process(data,source_socket):
+    """Implement this. Do something useful with the received data."""
+
+    if data[0] == "_":
+        if "_CONNECT_" in data:
+            print(data[9:] + " connected to the server!")
+            broadcast(source_socket,"Welcome to the server " + data[9:] + "!",True)
+    else:
+        print(data)
+        broadcast(source_socket,data,True)
+    sleep(1) # emulating processing time
+     
+class ProcessThread(Thread):
     def __init__(self):
+        super(ProcessThread, self).__init__()
+        self.running = True
+        self.q = Queue.Queue()
+        self.client_socket = None
+    def add(self, data, conn):
+        self.q.put(data)
+        self.client_socket = conn
+    def stop(self):
+        self.running = False
+ 
+    def run(self):
+        while self.running:
+            try:
+                # block for 1 second only:
+                value = self.q.get(block=True, timeout=1)
+                process(value,self.client_socket)
+                self.client_socket = None
+            except Queue.Empty:
+                pass
+                #sys.stdout.write('.')
+                #sys.stdout.flush()
+t = ProcessThread()
+t.start()
+
+class ClientThread(Thread):
+    def __init__(self,socket):
+        super(ClientThread, self).__init__()
+        self.running = True
+        self.conn = socket
+    def stop(self):
+        self.running = False 
+    def run(self):
+        while self.running:
+            try:
+                data = self.conn.recv(1024)
+                if data:
+                    #print data
+                    t.add(data,self.conn)
+            except socket.error, msg:
+                print("Socket error! %s" % msg)
+                pass
+        self.conn.close()
+    
+class Server():
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# Create a socket object
         self.host = GetIp()
-        self.port = 6119
-        self.s = socket.socket()
-        self.s.bind((self.host, self.port))
-        print("Hosting on IP: " + str(self.host) + "\r\nListening on port: " + str(self.port)+ "\r\n")
-        self.s.listen(5)
-
-        self.waiting_clients = []
-        self.clients = {}
-        self.clients[self.host] = self.s
-
-    def process(self,data,conn):
-        if not data:
-            return
-        if data[0] == "_":
-
-            if len(self.waiting_clients) > 0:
-                for i in self.waiting_clients:
-                    if i == conn.getpeername()[0]:
-                        self.clients = removekey(self.clients,i)
-                        self.clients[data[9:]] = conn
-                        self.waiting_clients.remove(i)
-            
-            if "_CONNECT_" in data:
-                print(data[9:] + " connected!")               
-            elif "_DISCONNECT_" in data:
-                print(data[12:] + " disconnected!")
-        else:
-            print(data)
-        sleep(0.2)
-        
-    def send_message(self,username,client_socket,data):
-        #client_socket.sendall(username + ": " + data)
-        client_socket.sendall(data)
-        sleep(0.2)
-        
-    def main(self):
+        self.port = 6119                 # Reserve a port for your service.
+        self.s.bind((self.host, self.port))        # Bind to the port
+        print(str(self.host) + " Listening on port : " + str(self.port)+"\r\n")
+     
+        self.s.listen(5)                 # Now wait for client connection.
+    def main(self):  
         while True:
-            inputReady, outputReady, exceptReady = select.select(self.clients.values(), [], [])
-            for x in inputReady:
-                if x != None:
-                    if x == self.s:
-                        try:
-                            conn, addr = self.s.accept()
-                            self.clients[addr[0]] = conn
-                            self.waiting_clients.append(addr[0])
-                            #self.clients[addr[0]] = thread.start_new_thread(client_thread ,(conn,)) 
-                        except socket.error, msg:
-                            print("Socket error: " + str(msg))
-                            pass
+            try:
+                conn, addr = self.s.accept()
 
-                    else:  
-                        data = x.recv(1024)
-                        if data:
-                            if not "." in data:
-                                self.process(data,x)
-                            for username,client_socket in self.clients.iteritems():
-                                if client_socket != None and client_socket is not self.s:
-                                    self.send_message(username,client_socket,data)
-        sys.exit(1)
-
+                thread = ClientThread(conn)
+                thread.start()
+                clients.append(thread)
+                
+            except socket.error, msg:
+                print("Socket error! %s" % msg)
+                pass
+        t.stop()
+        t.join()
+ 
+#########################################################
 if __name__ == "__main__":
     server = Server()
     server.main()
